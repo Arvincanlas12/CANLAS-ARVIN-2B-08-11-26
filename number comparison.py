a97ed6810ab78199220d4a28aecb96d1691736bf
Arvincanlas12 <canlasarvin678@gmail.com>
2026-08-11 11:30:22 +0800

    Add files via upload

---
+++ number comparison.py
@@ -0,0 +1,16 @@
+class Comparator:
+    def compare_three_numbers(self):
+        num1 = float(input("Enter your first number: "))
+        num2 = float(input("Enter your second number: "))
+        num3 = float(input("Enter your third number: "))
+
+        if num1 >= num2 and num1 >= num3:
+            print("num1 is the biggest number")
+        elif num2 >= num1 and num2 >= num3:
+            print("num2 is the biggest number")
+        else:
+            print("num3 is the biggest number")
+
+
+obj = Comparator()
+obj.compare_three_numbers()
